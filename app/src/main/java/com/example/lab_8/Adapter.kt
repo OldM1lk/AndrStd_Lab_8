0a78240aaa6aaeeccc52c302d1e847a2f3da29e5
OldM1lk <MrlaimWin@gmail.com>
2024-12-06 17:12:22 +0700

    Add branch and code for task2

--- app/src/main/java/com/example/lab_8/Adapter.kt
+++ app/src/main/java/com/example/lab_8/Adapter.kt
@@ -9,8 +9,16 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class Adapter(diffCallback: DiffCallback) : ListAdapter<WeatherEntry, Adapter.ViewHolder>(diffCallback) {
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class Adapter(diffCallback: DiffCallback) : ListAdapter<WeatherEntry, RecyclerView.ViewHolder>(diffCallback) {
+
+    private val VIEW_TYPE_HOT = 1
+    private val VIEW_TYPE_COLD = 2
+
+    override fun getItemViewType(position: Int): Int {
+        val tempCel = getItem(position).main.temp - 273.15
+        return if (tempCel >= 0) VIEW_TYPE_HOT else VIEW_TYPE_COLD
+    }
+    inner class ViewHolderHot(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(weatherEntry: WeatherEntry) {
             itemView.findViewById<TextView>(R.id.date).text = weatherEntry.dt_txt
             val tempCel = weatherEntry.main.temp - 273.15
@@ -22,15 +30,38 @@ class Adapter(diffCallback: DiffCallback) : ListAdapter<WeatherEntry, Adapter.Vi
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.forecast_items, parent, false)
-        return ViewHolder(view)
+    inner class ViewHolderCold(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(weatherEntry: WeatherEntry) {
+            itemView.findViewById<TextView>(R.id.date).text = weatherEntry.dt_txt
+            val tempCel = weatherEntry.main.temp - 273.15
+            itemView.findViewById<TextView>(R.id.temp).text = String.format("%.2f °C", tempCel)
+            val iconUrl = "https://openweathermap.org/img/wn/${weatherEntry.weather[0].icon}@2x.png"
+            Glide.with(itemView.context)
+                .load(iconUrl)
+                .into(itemView.findViewById(R.id.temp_icon))
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val forecastItem = getItem(position)
-        holder.bind(forecastItem)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_HOT -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.forecast_items_hot, parent, false)
+                ViewHolderHot(view)
+            }
+            VIEW_TYPE_COLD -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.forecast_items_cold, parent, false)
+                ViewHolderCold(view)
+            }
+            else -> throw IllegalArgumentException("Неизвестный тип представления $viewType")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val weatherEntry = getItem(position)
+        when (holder) {
+            is ViewHolderHot -> holder.bind(weatherEntry)
+            is ViewHolderCold -> holder.bind(weatherEntry)
+        }
     }
 }
 
